707a9378c1e2a1e6aba50e416b945b8e492e4469
Kapil Chopra <kapilchopra963@gmail.com>
2018-01-19 17:07:01 -0500

    Tutorial till 10

---
+++ app3.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+/*
+fs.readFile('sample.txt', 'utf-8', function(err, data){
+  console.log(data);
+  fs.writeFile('writeMe.txt', data);
+});
+*/
+
+//var readme = fs.readFileSync('sample.txt', 'utf-8');
+//console.log(readme);
+
+//fs.writeFileSync('writeMe.txt', readme);
+
+//fs.unlink('writeMe.txt');
+
+//fs.mkdirSync('mydir');
+//fs.rmdirSync('mydir');
+
+/*
+fs.mkdir('mydir', function(){
+  fs.readFile('sample.txt','utf-8', function(err,data){
+    fs.writeFile('./mydir/writeMe.txt', data);
+  });
+});
+*/
+/*
+fs.unlink('./mydir/writeMe.txt', function(){
+  fs.rmdir('mydir');
+});
+*/
